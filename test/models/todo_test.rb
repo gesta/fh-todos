2ff58119c166923c108f15b274dfc89d8f920b98
Gerasim Stanchev <gero@tutuf.com>
2019-04-05 15:36:41 +0300

    DRY up data from model tests

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,53 +1,40 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-  def default_attributes
-    {
-      completed?: false,
-      content: 'sample',
-      priority: 42,
-      due_date: Date.new(1999, 1, 25)
-    }
-  end
-
-  def build(extra = {})
-    Todo.new(default_attributes().merge(extra))
-  end
-
   test 'completed? has to be boolean' do
-    assert build({completed?: true}).valid?
-    assert build({completed?: false}).valid?
+    assert build_todo({completed?: true}).valid?
+    assert build_todo({completed?: false}).valid?
 
-    todo = build({completed?: nil}).tap(&:validate)
+    todo = build_todo({completed?: nil}).tap(&:validate)
     assert todo.errors.full_messages == ['Completed?  is not true or false']
   end
 
   test 'content has to be provided' do
-    todo = build({content: ''}).tap(&:validate)
+    todo = build_todo({content: ''}).tap(&:validate)
     assert todo.errors.full_messages == ["Content can't be blank"]
   end
 
   test 'content has to be no more than 200 characters' do
-    assert build({content: 'a' * 200}).valid?
+    assert build_todo({content: 'a' * 200}).valid?
 
-    todo = build({content: 'a' * 201}).tap(&:validate)
+    todo = build_todo({content: 'a' * 201}).tap(&:validate)
     assert todo.errors.full_messages == ["Content has to be up to 200 characters"]
   end
 
   test 'due_date has to be provided' do
-    todo = build({due_date: nil}).tap(&:validate)
+    todo = build_todo({due_date: nil}).tap(&:validate)
     assert todo.errors.full_messages == ["Due date can't be blank"]
   end
 
   test 'priority has to be provided' do
-    todo = build({priority: nil}).tap(&:validate)
+    todo = build_todo({priority: nil}).tap(&:validate)
     assert todo.errors.full_messages == ["Priority is not a number"]
   end
 
   test 'priority has to be at least 1' do
-    assert build({priority: 1}).valid?
+    assert build_todo({priority: 1}).valid?
 
-    todo = build({priority: 0}).tap(&:validate)
+    todo = build_todo({priority: 0}).tap(&:validate)
     assert todo.errors.full_messages == ['Priority must be greater than or equal to 1']
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,9 +4,34 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+module UserTestHelper
+  def default_user_attributes
+    {
+      admin?: false,
+      email: 'test@ema.il',
+      password: 'simpler',
+      password_confirmation: 'simpler'
+    }
+  end
 
-  # Add more helper methods to be used by all tests here...
+  def build_user(extra = {})
+    User.new(default_user_attributes().merge(extra))
+  end
+
+  def default_todo_attributes
+    {
+      completed?: false,
+      content: 'sample',
+      priority: 42,
+      due_date: Date.new(1999, 1, 25)
+    }
+  end
+
+  def build_todo(extra = {})
+    Todo.new(default_todo_attributes().merge(extra))
+  end
+end
+
+class ActiveSupport::TestCase
+  include UserTestHelper
 end
